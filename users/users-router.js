de7d90ff40f72d17adbc380b1b50f45c238c26e6
Daniel Weinman <namniewd@gmail.com>
2019-02-13 16:40:43 -0500

    added GET routes for all users and specific user

--- users/users-router.js
+++ users/users-router.js
@@ -4,4 +4,29 @@ const Users = require('./userDb.js');
 
 const router = express.Router();
 
+// GET all users
+router.get('/', (req, res) => {
+    Users.get()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The posts information could not be retrieved." });
+        })
+})
+
+// GET user by specific id 
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+
+    Users.getById(id)
+        .then(user => {
+            if (!user) {
+                res.status(404).json({ message: "The user with specified id does not exist" });
+            } else {
+                res.status(200).json(user)
+            }
+        })
+})
+
 module.exports = router;
